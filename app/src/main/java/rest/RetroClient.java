d8702574a6e852c11663c1e15c86b5da3f7fe24f
Gavin Hersteller <lezoig@gmail.com>
2018-08-31 03:12:04 +0200

    Retrofit instance
    Used to make api calls to the given url

--- app/src/main/java/rest/RetroClient.java
+++ app/src/main/java/rest/RetroClient.java
@@ -1,5 +1,15 @@
 package rest;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.util.Log;
+
+import java.util.List;
+
+import model.AndroidVersion;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -13,22 +23,36 @@ public class RetroClient {
      * URLS
      *******/
     public static final String ROOT_URL = "http://codetest.cobi.co.za";
+    private static Retrofit retrofit = null;
 
     /**
      * Get Retrofit Instance
      */
     private static Retrofit getRetrofitInstance(){
-        return new Retrofit.Builder()
-                .baseUrl(ROOT_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(ROOT_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
     }
-    /**
-     * Get API Service
-     *
-     * @return API Service
-     */
-    public static ApiService getApiService(){
-        return getRetrofitInstance().create(ApiService.class);
+
+    public LiveData<List<AndroidVersion>> getAndroidVersions(){
+        final MutableLiveData<List<AndroidVersion>> listMutableLiveData
+                = new MutableLiveData<>();
+        getRetrofitInstance().create(ApiService.class).getVersions()
+                .enqueue(new Callback<List<AndroidVersion>>() {
+                    @Override
+                    public void onResponse(Call<List<AndroidVersion>> call, Response<List<AndroidVersion>> response) {
+                        listMutableLiveData.postValue(response.body());
+                    }
+                    @Override
+                    public void onFailure(Call<List<AndroidVersion>> call, Throwable t) {
+                        Log.e("RetrofitClient", "Error getting Versions");
+                    }
+                });
+        return listMutableLiveData;
     }
 }
