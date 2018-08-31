4b12629853f1b1a21b427c046181e77841666078
Gavin Hersteller <lezoig@gmail.com>
2018-08-31 03:13:36 +0200

    Main Activity
    Changed to use databinding with LiveData

--- app/src/main/java/com/cobi/codingassignment/MainActivity.java
+++ app/src/main/java/com/cobi/codingassignment/MainActivity.java
@@ -1,52 +1,54 @@
 package com.cobi.codingassignment;
 
 import android.app.ProgressDialog;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Toast;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 import adapter.VersionAdapter;
-import app.Versions;
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import model.AndroidVersion;
-import rest.ApiService;
-import rest.RetroClient;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import util.DividerItemDecoration;
+import viewmodel.VersionsViewModel;
+
+import com.cobi.codingassignment.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     public static final String KEY = "version";
     public static final String TAG = MainActivity.class.getSimpleName();
 
     // views
-    @BindView(R.id.toolbar) Toolbar toolbar;
-    @BindView(R.id.recycler_view) RecyclerView recyclerView;
+    Toolbar toolbar;
+    RecyclerView recyclerView;
 
     private View parentView;
-    private List<AndroidVersion> androidVersions = new ArrayList<AndroidVersion>();
+    private List<AndroidVersion> androidVersions = new ArrayList<>();
     private VersionAdapter adapter;
+
+    VersionsViewModel versionsViewModel;
+
+    ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        // bind views
-        ButterKnife.bind(this);
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        versionsViewModel = ViewModelProviders.of(this).get(VersionsViewModel.class);
+        mainBinding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mainBinding.recyclerView.hasFixedSize();
+        mainBinding.recyclerView.addItemDecoration(new DividerItemDecoration(this));
+
         // Set Toolbar
         setSupportActionBar(toolbar);
 
@@ -99,33 +101,11 @@ public class MainActivity extends AppCompatActivity {
         dialog.setTitle("Loading");
         dialog.setMessage("Please wait");
         dialog.show();
-        //Creating an object of our api interface
-        ApiService api = RetroClient.getApiService();
-        //Calling JSON
-        Call<Versions> call = api.getVersionsJSON();
-        //Enqueue Callback will be call when get response
-        call.enqueue(new Callback<Versions>() {
-            @Override
-            public void onResponse(Call<Versions> call, Response<Versions> response) {
-                //Dismiss dialog
-                dialog.dismiss();
-                Log.d(TAG, response.toString());
-                if(response.isSuccessful()){
-                    androidVersions = response.body().getVersions();
-                    //Binding the List to Adapter
-                    adapter = new VersionAdapter(androidVersions, R.layout.list_item, MainActivity.this);
-                    recyclerView.setAdapter(adapter);
-                }else{
-                    Toast.makeText(MainActivity.this, R.string.cannot_load_data, Toast.LENGTH_LONG).show();
-                }
-            }
-            @Override
-            public void onFailure(Call<Versions> call, Throwable t) {
-                dialog.dismiss();
-                Log.d("Error", t.getMessage());
-                Toast.makeText(MainActivity.this, getString(R.string.no_internet_connection),
-                        Toast.LENGTH_LONG).show();
-            }
+
+        versionsViewModel.getListLiveData().observe(this, androidVersions ->{
+            adapter = new VersionAdapter(androidVersions, R.layout.list_item, this);
+            mainBinding.recyclerView.setAdapter(adapter);
+            dialog.dismiss();
         });
     }
 }
